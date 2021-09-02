fa156bd6a98b3e5cf6340a0146491b700fc14232
onionsalot <trong.nguyen1008@gmail.com>
2021-09-02 16:50:22 -0400

    adds delete functionality to categories

--- controllers/api/categories.js
+++ controllers/api/categories.js
@@ -3,6 +3,7 @@ const Category = require('../../models/category');
 module.exports = {
     getAll,
     create,
+    delete: deleteOne,
   };
 
 
@@ -21,6 +22,17 @@ module.exports = {
     try {
         const category = await Category.create(req.body);
         res.status(201).json(category);
+    } catch (err) {
+      // Client will check for non-2xx status code
+      // 400 code  = bad request
+      res.status(400).json(err);
+    }
+  }
+
+  async function deleteOne(req, res) {
+    try {
+        const deletedItemID = await Category.findByIdAndRemove(req.params.id);
+        res.status(200).json(deletedItemID)
     } catch (err) {
       // Client will check for non-2xx status code
       // 400 code  = bad request
--- routes/api/categories.js
+++ routes/api/categories.js
@@ -4,5 +4,6 @@ const CategoriesCtrl = require('../../controllers/api/categories');
 
 router.get('/', CategoriesCtrl.getAll);
 router.post('/create', CategoriesCtrl.create);
+router.delete('/delete/:id', CategoriesCtrl.delete)
 
 module.exports = router;
