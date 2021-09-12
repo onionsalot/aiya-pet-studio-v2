7ed8cc618851def3494238506f5a5979ec654f44
onionsalot <trong.nguyen1008@gmail.com>
2021-09-12 12:39:26 -0400

    Separates the item schema as well as disabling the automatically created _ids for each new cart item

--- models/cart.js
+++ models/cart.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const itemsSchema = new Schema({
+    item: { type: Schema.Types.ObjectId, ref: "Item"},
+            quantity: Number,
+}, { _id: false });
+
 const cartSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User"},
-    items: [
-        {
-            item: { type: Schema.Types.ObjectId, ref: "Item"},
-            quantity: Number,
-        }
-    ],
+    items: [itemsSchema],
     paid: { type: Boolean, default: false }
 }, { timestamps: true });
 
