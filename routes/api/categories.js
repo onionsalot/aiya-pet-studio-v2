f307ac8be8f184a750da5b7c36a2235b7a545530
onionsalot <trong.nguyen1008@gmail.com>
2021-09-12 14:06:33 -0400

    defines the routes that need to be protected by a login

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const CategoriesCtrl = require('../../controllers/api/categories');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get('/', CategoriesCtrl.getAll);
-router.post('/create', CategoriesCtrl.create);
-router.delete('/delete/:id', CategoriesCtrl.delete)
-router.put('/update/:id', CategoriesCtrl.update); // update name
+router.post('/create', ensureLoggedIn, CategoriesCtrl.create);
+router.delete('/delete/:id', ensureLoggedIn, CategoriesCtrl.delete)
+router.put('/update/:id', ensureLoggedIn, CategoriesCtrl.update); // update name
 // router.put('/addItem/:id/:iid', CategoriesCtrl.addItem);
 
 module.exports = router;
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const itemsCtrl = require('../../controllers/api/items');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get('/', itemsCtrl.getAll);
-router.post('/create', itemsCtrl.create);
-router.delete('/delete/:id', itemsCtrl.delete);
-router.put('/update/:id', itemsCtrl.update);
+router.post('/create', ensureLoggedIn, itemsCtrl.create);
+router.delete('/delete/:id', ensureLoggedIn, itemsCtrl.delete);
+router.put('/update/:id', ensureLoggedIn, itemsCtrl.update);
 
 module.exports = router;
