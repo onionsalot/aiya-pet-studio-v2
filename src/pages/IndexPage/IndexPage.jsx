aa8ce0c8055f4172ce14cf0cf197987d6e540727
onionsalot <trong.nguyen1008@gmail.com>
2021-09-14 16:53:14 -0400

    Hoists item state to top level for easier access to all components

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,17 +1,7 @@
 import { useEffect, useState } from "react";
-import * as itemsAPI from "../../utilities/items-api";
 import "./IndexPage.scss";
 
 export default function IndexPage() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    async function getItem() {
-      const response = await itemsAPI.getAll();
-      setItems(response);
-      console.log(response)
-    }
-    getItem();
-  }, []);
 
 
   return (
