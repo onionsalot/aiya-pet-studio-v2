94e7b90118ccaff37fc916ee96d41fbba7adf368
onionsalot <trong.nguyen1008@gmail.com>
2021-09-15 15:49:42 -0400

    renders all elements if no category is clicked

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -6,10 +6,7 @@ export default function ItemList({ items, currentCategory }) {
 
     useEffect(() => {
         console.log("rendered itemlist", currentCategory)
-        const currentItems = items.filter(e => e.category.name === currentCategory).map((e,idx) => {
-            return <ItemCard item={ e } key= { idx }/>
-        }
-        )
+        const currentItems = currentCategory ? items.filter(e => e.category.name === currentCategory).map((e,idx) => <ItemCard item={ e } key={ idx }/>) : items.map((e,idx) => <ItemCard item={ e } key={ idx }/>)
 
         setDisplay(currentItems)
       },[currentCategory, items])
